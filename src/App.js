e7b28b63193fde1235c7e21e0a69957283b7aeef
vadgab9@gmail.com <vadgab9@gmail.com>
2024-03-10 21:43:09 +0500

    add code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import Collection from './collection'
+import data from './data.json'
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+
 
 function App() {
+    const [collections, setCollections] = React.useState([]);
+    const [category,setCattegory]=useState([]);
+    const [searchInput,setSearchInput]=useState('');
+    const [changeTab,setChangeTab]=useState(0);
+    useEffect(() => {
+        // Поскольку данные уже импортированы, вы можете напрямую использовать переменную `data`
+        setCollections(data.collections);
+        setCattegory(data.categories)
+        console.log(data.collections);
+    }, []);
+
+    function onChangeTab(index){
+        setChangeTab(index)
+    }
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+            {
+                category.map((cat,index)=> (
+                    <li onClick={()=>onChangeTab(index)} key={index} className={changeTab === index ? 'active' : ''}>{cat.name}</li>
+                ))
+            }
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input value={searchInput} onChange={e => setSearchInput(e.target.value)} className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+          {
+              collections.filter((obj)=>{
+                  return obj.name.toLowerCase().includes(searchInput.toLowerCase())
+              }).filter((obj)=>{
+                  if (changeTab==0) {
+                      return true
+                  }else {
+                      return obj.category === changeTab;
+                  }
+              }).map((obj,index)=>(
+                  <Collection
+                      category={changeTab}
+                      key={index}
+                      name={obj.name}
+                      images={obj.photos}
+                  />
+              ))
+          }
+
       </div>
       <ul className="pagination">
         <li>1</li>
